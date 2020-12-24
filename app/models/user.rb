8ee0acfda3b86813b22afd4809c16b1090c50cc5
yuyakiso <yuya2226@gmail.com>
2020-12-24 16:01:23 +0900

    バリデーションの設定

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :birth_day, presence: true
+
+  has_many :items
+  has_many :buy_logs
+end
