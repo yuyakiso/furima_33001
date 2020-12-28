1badfdf158f418e345fc48980bf45e595ee565fe
yuyakiso <yuya2226@gmail.com>
2020-12-28 19:10:17 +0900

    ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
 
-  resources :items, only: [:new, :create,]
+  resources :items, only: [:new, :create, :show]
 end
