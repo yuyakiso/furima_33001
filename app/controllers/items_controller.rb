d6a2e8a7a7a1dc672ca6db9ac31647feddb3c5de
yuyakiso <yuya2226@gmail.com>
2020-12-28 14:29:46 +0900

    ビューの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,25 @@
 class ItemsController < ApplicationController
+before_action :authenticate_user!, only: :new
   def index
-    
+    @items = Item.includes(:user)
   end
 
   def new
-    
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :message, :category_id, :status_id, :charge_id, :schedule_id, :prefecture_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
