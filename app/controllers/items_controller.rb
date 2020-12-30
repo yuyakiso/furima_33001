bc1863584ac7f12412a0b497f2fab9a9f1bf5cb8
yuyakiso <yuya2226@gmail.com>
2020-12-30 19:15:36 +0900

    指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,6 +41,9 @@ class ItemsController < ApplicationController
     end
 
     def destroy
+      if @item.user_id != current_user.id
+        redirect_to action: :index
+      end
       item = Item.find(params[:id])
       item.destroy
       redirect_to root_path
