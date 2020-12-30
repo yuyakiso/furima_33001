fb85e7f9ba77e16d4749afe2ed00d3412862edfb
yuyakiso <yuya2226@gmail.com>
2020-12-30 19:05:32 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,6 +39,12 @@ class ItemsController < ApplicationController
         render :edit
       end
     end
+
+    def destroy
+      item = Item.find(params[:id])
+      item.destroy
+      redirect_to root_path
+    end
   
     private
     def item_params
