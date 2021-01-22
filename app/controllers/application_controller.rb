90dbf18f6bce5d5aee6bbaeede192d83846bb477
yuyakiso <yuya2226@gmail.com>
2021-01-22 18:07:10 +0900

    basic認証テスト時無効

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  before_action :basic_auth if Rails.env.production?
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
